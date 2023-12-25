d69286c8fce5b9019a5c6b5c30a9f6c0417912a5
xupengfei <faberxu@gmail.com>
2023-12-25 17:21:37 +0800

    threejs2/DemoTwo07: react-spring东湖

--- src/pages/threejs2/DemoTwo07React.tsx
+++ src/pages/threejs2/DemoTwo07React.tsx
@@ -12,31 +12,33 @@ let animate1: gsap.core.Tween|undefined = undefined
 function Scene() {
   const boxRef = useRef<THREE.Mesh>(null!);
 
-  useEffect(() => {
-    console.log('boxRef.current', boxRef.current)
-    animate1 = gsap.to(boxRef.current.position, {
-      x: 5,
-      duration: 5,
-      ease: "power1.inOut",
-      //   设置重复的次数，无限次循环-1
-      repeat: -1,
-      //   往返运动
-      yoyo: true,
-      //   delay，延迟2秒运动
-      delay: 2,
-      onComplete: () => {
-        console.log("动画完成");
-      },
-      onStart: () => {
-        console.log("动画开始");
-      },
-    });
-    gsap.to(boxRef.current.rotation, { x: 2 * Math.PI, duration: 5, ease: "power1.inOut" });
-  }, [boxRef.current])
-
   return (
     <>
-      <mesh ref={boxRef} rotation={[Math.PI / 4, 0, 0]}>
+      <mesh
+        ref={boxRef}
+        rotation={[Math.PI / 4, 0, 0]}
+        onUpdate={self => {
+          console.log('onUpdate', self)
+          animate1 = gsap.to(self.position, {
+            x: 5,
+            duration: 5,
+            ease: "power1.inOut",
+            //   设置重复的次数，无限次循环-1
+            repeat: -1,
+            //   往返运动
+            yoyo: true,
+            //   delay，延迟2秒运动
+            delay: 2,
+            onComplete: () => {
+              console.log("动画完成");
+            },
+            onStart: () => {
+              console.log("动画开始");
+            },
+          });
+          gsap.to(self.rotation, { x: 2 * Math.PI, duration: 5, ease: "power1.inOut" });
+        }}
+      >
         <boxGeometry args={[1,1,1]} />
         <meshBasicMaterial color={0xffff00} />
       </mesh>
@@ -45,7 +47,7 @@ function Scene() {
 }
 
 export default function DemoTwo07React() {
-  // const [animated, setAnimated] = useState(true)
+  const [animated, setAnimated] = useState(true)
 
   function handleClick() {
     // console.log('animated', animated, animate1, animate1?.isActive())
@@ -57,6 +59,7 @@ export default function DemoTwo07React() {
       //   恢复
       animate1.resume();
     }
+    setAnimated(!animated)
   }
 
   return (
@@ -72,7 +75,7 @@ export default function DemoTwo07React() {
       </Canvas>
 
       <Button onClick={() => handleClick()}>
-        动画中
+        {animated ? '暂停' : '恢复'}
       </Button>
     </div>
   )
