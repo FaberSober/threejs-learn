fdaf378a488008a60c5fecd6e5836c5d1bc3c2ac
xupengfei <faberxu@gmail.com>
2023-12-27 18:26:09 +0800

    threejs2/DemoTwo15: 第三人称实现：PointLockControl，键盘控制人物移动（React实现）

--- src/pages/threejs2/DemoTwo06React.tsx
+++ src/pages/threejs2/DemoTwo06React.tsx
@@ -3,9 +3,12 @@ import * as THREE from 'three';
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Box, GizmoHelper, GizmoViewport, Line, OrbitControls, PerspectiveCamera, PointerLockControls, useHelper } from "@react-three/drei";
 import MyFactory from "@/components/modal/myFactory/MyFactory";
-import { Checkbox, Radio, Space } from "antd";
+import { Button, Checkbox, Radio, Space } from "antd";
 import MyHelper from "@/components/modal/MyHelper";
 import MyCar from "@/components/modal/car/MyCar";
+import XbotModel, { Anim } from "@/components/modal/bot/XbotModel";
+import { OrbitControls as OrbitControlsImpl } from "three-stdlib/controls/OrbitControls";
+import gsap from "gsap";
 
 
 // Create a sine-like wave
@@ -89,6 +92,8 @@ function Scene() {
   const light1 = React.useRef<THREE.DirectionalLight>(null!)
   const light1ShadowCamera = useRef<THREE.OrthographicCamera>(null!)
 
+  const botBoxRef = useRef<THREE.Mesh>(null!); // 人物
+
   useHelper(showHelper && cameraRef, THREE.CameraHelper)
   useHelper(showHelper && light1, THREE.DirectionalLightHelper)
   // useHelper(showHelper && light1ShadowCamera, THREE.CameraHelper)
@@ -107,7 +112,13 @@ function Scene() {
       <MyFactory showHelper={showHelper}/>
 
       {/* 小车 */}
-      <MovingCar />
+      <MovingCar/>
+
+      {/* 人物 */}
+      <object3D ref={botBoxRef} position={[0, 0, 0]} scale={[2,2,2]}>
+        <XbotModel selAnim={Anim.idle}/>
+        {/*<MyHelper size={5} unit={20}/>*/}
+      </object3D>
 
       {/* 场景摄像机 */}
       <PerspectiveCamera
@@ -139,10 +150,41 @@ export default function DemoTwo06React() {
   const [controlType, setControlType] = useState<'Orbit' | 'PointerLock'>('Orbit')
   const [cameraType, setCameraType] = useState<CameraType>(CameraType.Global)
   const [showHelper, setShowHelper] = useState(true)
+  const cameraRef = useRef<THREE.PerspectiveCamera>(null!)
+  const controlRef = useRef<OrbitControlsImpl>(null!)
 
   const contextValue: ConfigLayoutContextProps = {
     cameraType,
     showHelper,
+  }
+
+  /**
+   * 查看全局场景
+   */
+  function handleViewGlobal() {
+    gsap.to(cameraRef.current.position, { x: 40, y: 40, z: 40, duration: 3, ease: "power1.inOut", });
+    gsap.to(controlRef.current.target, { x: 0, y: 0, z: 0, duration: 3, ease: "power1.inOut", });
+  }
+
+  /**
+   * 查看楼栋场景
+   */
+  function handleViewBuilding() {
+    gsap.to(cameraRef.current.position, { x: 14, y: 14, z: 8, duration: 3, ease: "power1.inOut", });
+    gsap.to(controlRef.current.target, { x: 0, y: 5, z: -8, duration: 3, ease: "power1.inOut", });
+  }
+
+  /**
+   * 查看摄像头场景
+   */
+  function handleViewCamera() {
+    gsap.to(cameraRef.current.position, { x: 1, y: 16, z: 19, duration: 3, ease: "power1.inOut", });
+    gsap.to(controlRef.current.target, { x: -12, y: 6, z: 10, duration: 3, ease: "power1.inOut", });
+  }
+
+  function handlePrintInfo() {
+    console.log('控制器target位置：', controlRef.current.target)
+    console.log('相机位置：', cameraRef.current.position)
   }
 
   return (
@@ -154,17 +196,15 @@ export default function DemoTwo06React() {
             <Scene/>
           </ConfigLayoutContext.Provider>
 
-          <Box args={[1,1,1]} position={[0,1,0]} material-color={0x00FF00} />
-
           {/* 全局摄像机 */}
           <PerspectiveCamera
+            ref={cameraRef}
             makeDefault={cameraType === CameraType.Global}
             position={[30, 30, 30]}
           />
 
-          {controlType === 'Orbit' && <OrbitControls/>}
-          {controlType === 'PointerLock' && <PointerLockControls/>}
-
+          <OrbitControls ref={controlRef} enabled={controlType === 'Orbit'} />
+          {/*<PointerLockControls enabled={controlType === 'PointerLock'} />*/}
 
           <GizmoHelper alignment='bottom-right' margin={[100, 100]}>
             <GizmoViewport/>
@@ -187,6 +227,18 @@ export default function DemoTwo06React() {
               <Radio.Button value={CameraType.Scene}>场景摄像机</Radio.Button>
               <Radio.Button value={CameraType.Car}>移动小车</Radio.Button>
             </Radio.Group>
+          </Space>
+
+          <Space style={{marginTop: 12}}>
+            <div>查看场景：</div>
+            <Button onClick={handleViewGlobal}>全局</Button>
+            <Button onClick={handleViewBuilding}>楼栋</Button>
+            <Button onClick={handleViewCamera}>摄像头</Button>
+          </Space>
+
+          <Space style={{marginTop: 12}}>
+            <div>获取数据：</div>
+            <Button onClick={handlePrintInfo}>获取数据</Button>
           </Space>
 
           <Space style={{marginTop: 12}}>
